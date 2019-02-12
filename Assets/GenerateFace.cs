7f7b31c3c716f63a1f54190e3bf890fa4f26bb37
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-02-12 17:04:41 +0100

    add procedural generation examples and fix mesh array

--- Assets/GenerateFace.cs
+++ Assets/GenerateFace.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class GenerateFace : MonoBehaviour {
 
     public Mesh mesh;
-    public int meshGridXSize, meshGridYSize;
-    Mesh[] meshArray;
+    public int vertexGridXSize, vertexGridYSize;
+    Vector3[] vertexArray;
 
 
     void Start () {
@@ -15,29 +15,39 @@ public class GenerateFace : MonoBehaviour {
 
         mesh.Clear();
 
-        meshArray = new Mesh[meshGridXSize * meshGridYSize];
+        //vertexArray = new Mesh[vertexGridXSize * vertexGridYSize];
         
         /*mesh.vertices = new Vector3[] { new Vector3(-1, 0, 0), new Vector3(0, 2, 0), new Vector3(1, 0, 0) };
         mesh.triangles = new int[] { 0, 1, 2 };
         mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };*/
 
-        mesh.vertices = new Vector3[] { new Vector3(1, 0, 0), new Vector3(0, 2, 0), new Vector3(-1, 0, 0) };
-        mesh.triangles = new int[] { 0, 1, 2 };
-        mesh.uv = new Vector2[] { new Vector2(1, 1), new Vector2(0, 1), new Vector2(0, 0) };
+        
 
-        /*
-        for (int y = 0; y < meshGridYSize; y++)
+
+        /*for (int y = 0; y < meshGridYSize; y++)
         {
             for (int x = 0; x < meshGridXSize; x++)
             {
+                meshArray[y * x + x - 1] = new Mesh();
                 meshArray[y * x + x - 1] = GetComponent<MeshFilter>().mesh;
-                meshArray[y * x + x - 1].Clear();
+                //meshArray[y * x + x - 1].Clear();
                 meshArray[y * x + x - 1].vertices = new Vector3[] { new Vector3(x, 0, 0), new Vector3(0, 2 * y, 0), new Vector3(-x, 0, 0) };
                 meshArray[y * x + x - 1].triangles = new int[] { 0, 1, 2 };
                 meshArray[y * x + x - 1].uv = new Vector2[] { new Vector2(1, 1), new Vector2(0, 1), new Vector2(0, 0) };
             }
+        }*/
+
+        for (int y = 0; y < vertexGridYSize; y++)
+        {
+            for (int x = 0; x < vertexGridXSize; x++)
+            {
+                vertexArray[y * x + x - 1] = new Vector3(0, 0, 0);
+            }
         }
-        */
+
+        mesh.vertices = vertexArray;
+        mesh.triangles = new int[] { 0, 1, 2 };
+        mesh.uv = new Vector2[] { new Vector2(1, 1), new Vector2(0, 1), new Vector2(0, 0) };
     }
 
     private void OnDrawGizmos()
@@ -50,6 +60,5 @@ public class GenerateFace : MonoBehaviour {
             Gizmos.DrawSphere(mesh.vertices[i], .2f);
         }
     }
-
-
+    
 }
