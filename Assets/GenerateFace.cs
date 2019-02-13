53df5c53bd5868ef75de798f396d8f3c88b507a7
takkiemon <takkie91@hotmail.com>
2019-02-13 15:27:35 +0100

    dinges

--- Assets/GenerateFace.cs
+++ Assets/GenerateFace.cs
@@ -43,20 +43,30 @@ public class GenerateFace : MonoBehaviour {
         }
         */
 
-        
+
+        Debug.Log("vertexArray.Length: " + vertexArray.Length);
+        Debug.Log("triangles.Length: " + triangles.Length);
+        Debug.Log("vertexGridXSize: " + vertexGridXSize);
+
         for (int i = 0, j = 0; i < triangles.Length; i += 3, j++)
         {
+            Debug.Log("i: " + i + ", j: " + j + ".");
             triangles[i] = j;
             if (j + 1 < vertexArray.Length)
             {
+                Debug.Log("1st 'if' of i: " + i);
                 triangles[i + 1] = j + 1;
             }
             if (j + 2 < triangles.Length && j + vertexGridXSize < vertexArray.Length)
             {
+                Debug.Log("2nd 'if' of i: " + i);
                 triangles[i + 2] = j + vertexGridXSize;
+                //triangles[i + 2] = j + 2;
             }
+            Debug.Log("end of i: " + i + ", j: " + j + ".");
         }
-        
+        Debug.Log("testes002");
+
 
         mesh.vertices = vertexArray;
         mesh.triangles = triangles;
